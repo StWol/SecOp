62ee012fc31f47122bb193d536f039995c17572d
porschi <pp009@hdm-stuttgart.de>
2012-06-06 17:20:11 +0200

    problem

--- trend.py
+++ trend.py
@@ -97,6 +97,16 @@ def plot_future(prognose_kurs, prognose_datum,color):
     #ax.fill_between(prognose_datum, prognose_kurs + 1.9600 * sigma, prognose_kurs - 1.9600 * sigma, alpha=0.35, linestyle='dashed' , color=color)
 
     ax.hold(True)
+
+def plot_trend(prognose_datum,quote,last_kurs):
+    trend = []
+    for i in range(0,len(prognose_datum)):
+        trend.append(last_kurs)
+    ax = fig.add_subplot(111)
+    ax.plot_date(prognose_datum, trend, '-', color='#ebd82d',linewidth=4)
+    ax.hold(True)
+    ax = fig.add_subplot(111)
+
     
 ################################################################################    
 def plot_analyst(kurse, avg, daten,trefferquote):
@@ -133,12 +143,10 @@ cp=input("Für welches Unternehmen?\n")
 
 
 
-sql2 = """SELECT avg, zieldatum FROM analyst_avg_2 WHERE unternehmen = %d  AND `zieldatum`> '2010-01-01' ORDER BY avg_datum """ %(cp)
-sql3 = """SELECT neues_kursziel, zieldatum, analyst, avg FROM analyst_avg_2 WHERE unternehmen = %d  AND avg_datum> '2010-01-01' AND avg_datum<(SELECT CURDATE()) ORDER BY avg_datum, zieldatum """ %(cp)
 
 sql = "SELECT AVG( close ) , `datum` FROM kursdaten WHERE unternehmen =%d GROUP BY YEAR( `datum` ) , MONTH( `datum` )" %(cp)
 
-sql4 = """SELECT neues_kursziel, zieldatum, analyst FROM prognose
+sql4 = """SELECT neues_kursziel, zieldatum, analyst,neue_einstufung FROM prognose
  WHERE unternehmen = %d
  AND `zieldatum`>(SELECT CURDATE()) AND neues_kursziel >0
  ORDER BY zieldatum""" %(cp)
@@ -146,7 +154,7 @@ sql4 = """SELECT neues_kursziel, zieldatum, analyst FROM prognose
  
 sql5 = """SELECT neue_einstufung,analyst FROM prognose
  WHERE unternehmen = %d
- AND `zieldatum`>(SELECT CURDATE()) 
+ AND `zieldatum`>(SELECT CURDATE()) AND neues_kursziel >0
  ORDER BY zieldatum""" %(cp)
 
 
@@ -155,7 +163,6 @@ trefferquoten_dict = tq.start_company()
 
 
 avg_kurse = get_select(sql)
-ziel_kurse = get_select(sql3)
 prognose = get_select(sql4)
 einstufung = get_select(sql5)
 avg = [q[0] for q in avg_kurse]
@@ -163,13 +170,10 @@ avg = [q[0] for q in avg_kurse]
 datum_avg = [q[1] for q in avg_kurse]
 datum_avg =dates.date2num(datum_avg)
 
-datum_ziel = [q[1] for q in ziel_kurse]
-datum_ziel =dates.date2num(datum_ziel)
 
 datum_prognose = [q[1] for q in prognose]
 datum_prognose = dates.date2num(datum_prognose)
 
-analysten_list = [q[2] for q in ziel_kurse]
 #avg_2 = [q[3] for q in ziel_kurse]
 
 buy = 0
@@ -237,20 +241,13 @@ print "Mittlere Trefferquote derer die Neutral sagen: %f" %(t_q_neutral)
 
 analysten_dict ={}
 
-for row in ziel_kurse:
-    analysten_dict[row[2]] = []
-    
-for row in ziel_kurse:  
-    value = analysten_dict[row[2]]
-    value.append([row[0],row[3] ,dates.date2num(row[1])])
-
 analysten_prognosen_dict ={} 
 
 for row in prognose:
     analysten_prognosen_dict[row[2]] = []
 for row in prognose:  
     value = analysten_prognosen_dict[row[2]]
-    value.append([row[0],dates.date2num(row[1])])
+    value.append([row[0],dates.date2num(row[1]), row[3]])
     
 #date1 = datetime.date( 2006, 1, 31 )
 #date2 = datetime.date( 2012, 5, 21 )
@@ -264,41 +261,37 @@ ax = fig.add_subplot(111)
 ax.xaxis.set_major_locator(months)
 ax.xaxis.set_major_formatter(monthsFmt)
 ax.hold(True)
-ax = fig.add_subplot(111)
-ax.plot_date(datum_avg, avg,'-',color='black',label='tats. Kurs',linewidth=2)
-ax.hold(True)
-################################################################################
-### Die Schleife läuft jeden Analysten durch und ruft die methode zum zeichnen auf
-#
-for k,v in analysten_dict.iteritems():
-    kurse = [q[0] for q in v]
-    avgs = [q[1] for q in v]
-    daten = [q[2] for q in v]
-    #prepare_plotting(kurse, avgs, daten,analysten_prognosen_dict,k)
-    #plot_analyst(kurse, avgs, daten)
-    trefferquote = trefferquoten_dict[cp][k][2]
-    anzahl_ = trefferquoten_dict[cp][k][0]
-    if trefferquote > 0. and anzahl_ >5:
-        col_sig_list = plot_analyst(kurse, avgs, daten,trefferquote) 
-        color = col_sig_list
-        #sigma = col_sig_list[1]
-        if k in analysten_prognosen_dict.keys():    
-            val = analysten_prognosen_dict[k]
-            prognose_kurs=[] 
-            prognose_datum = []
-            for i in val:
-                
-                prognose_kurs.append(i[0])
-                prognose_datum.append(i[1])
-            plot_future(prognose_kurs, prognose_datum,color)
+
+#################################################################################
+#### Die Schleife läuft jeden Analysten durch und ruft die methode zum zeichnen auf
+##
+
+mittel_buy = 0.
+mittel_sell = 0.
+mittel_neutral = 0.
+
+count_buy = 0.
+count_sell = 0.
+count_neutral = 0.
 
 for k in analysten_prognosen_dict.keys():
-    if k not in analysten_dict.keys():
-        print "unbekannt: ",k
         val = analysten_prognosen_dict[k]
         prognose_kurs=[] 
         prognose_datum = []
-        color ='#000000' 
+        if val[0][2] == 1:        
+            color ='#00FF00'
+            mittel_buy += val[0][0]
+            count_buy += 1
+        elif val[0][2] == 2:
+            color ='#FF0000'
+            mittel_sell += val[0][0]
+            count_sell += 1
+        elif val[0][2] == 3:
+            color ='#FFFF00'
+            mittel_neutral += val[0][0]
+            count_neutral += 1
+        
+        
         #sigma_prog = []
         for i in val:
             
@@ -307,6 +300,33 @@ for k in analysten_prognosen_dict.keys():
          #   sigma_prog.append(1.)
         #print sigma_prog
         plot_future_unbekannt(prognose_kurs, prognose_datum,color)
+
+
+
+mittel_neutral = float(mittel_neutral)/count_neutral
+mittel_sell = float(mittel_sell)/count_sell
+mittel_buy = float(mittel_buy)/count_buy
+
+
+#print buy
+#print buy_clean
+#print count_buy
+
+print sell
+print sell_clean
+print count_sell
+
+ax = fig.add_subplot(111)
+ax.plot_date(datum_avg, avg,'-',color='black',label='tats. Kurs',linewidth=2)
+ax.hold(True)
+
+plot_trend(datum_prognose,t_q_neutral,mittel_neutral)
+plot_trend(datum_prognose,t_q_buy,mittel_buy)
+plot_trend(datum_prognose,t_q_sell,mittel_sell)
+
+
+
+
     
 ax.xaxis.set_major_locator(months)
 ax.xaxis.set_major_formatter(monthsFmt)
